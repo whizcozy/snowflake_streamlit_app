3566d1ab983dd3ec81c8b5bc83b12ceac7e94b4e
whizcozy <68869522+whizcozy@users.noreply.github.com>
2023-05-16 19:40:11 +0530

    Update streamlit_app.py
    multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,13 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
+streamlit.header('Build your own Fruit Smoothie')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#Make Fruit column as index column
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# Add multiselect option to pic the fruits for smoothie
+streamlit.multiselect('Pack some fruits:', list(my_fruit_list.index))
+
+#Display the table on the page
 streamlit.dataframe(my_fruit_list)
