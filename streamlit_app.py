1ec36595bc315d6f30ecb045811027271bd3a0df
whizcozy <68869522+whizcozy@users.noreply.github.com>
2023-05-16 19:57:27 +0530

    Update streamlit_app.py
    handled bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,10 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 #We want to filter the table data based on the fruits a customer will choose, so we'll pre-populate the list to set an example for the customer. 
 streamlit.multiselect('Pack some fruits:', list(my_fruit_list.index),['Avocado','Strawberries'])
 
+#Display the table on the page
+streamlit.dataframe(my_fruit_list)
+
 #show the selected fruits
 fruits_selected = streamlit.multiselect('Pack some fruits:', list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-#Display the table on the page
 streamlit.dataframe(fruits_to_show)
