48e2c4587aa108e98eacac0fe93c13b76081b14a
whizcozy <68869522+whizcozy@users.noreply.github.com>
2023-05-16 19:43:51 +0530

    Update streamlit_app.py
    Default selection of fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,8 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Add multiselect option to pic the fruits for smoothie
-streamlit.multiselect('Pack some fruits:', list(my_fruit_list.index))
+#We want to filter the table data based on the fruits a customer will choose, so we'll pre-populate the list to set an example for the customer. 
+streamlit.multiselect('Pack some fruits:', list(my_fruit_list.index),['Avocado','Strawberries'])
 
 #Display the table on the page
 streamlit.dataframe(my_fruit_list)
