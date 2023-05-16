64177bd7dfd5d9c05c56194acfc0c34209a89afb
whizcozy <68869522+whizcozy@users.noreply.github.com>
2023-05-16 13:48:52 +0530

    Update streamlit_app.py
    Breakfast Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
-streamlit.title('My name is Chandan Sharma')
+streamlit.title('My parents Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit.text('Idly Vada')
+streamlit.text('Puri and Sagu')
