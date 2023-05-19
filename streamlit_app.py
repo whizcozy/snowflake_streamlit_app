a6ddbc025be5b55b3011033e2169cd8cb349930e
whizcozy <68869522+whizcozy@users.noreply.github.com>
2023-05-20 01:11:34 +0530

    Update streamlit_app.py
    add fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,7 +65,7 @@ if streamlit.button('Get Fruit Load List'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
   
-#streamlit.stop()  
+
 
 
 def insert_row_snowflake(new_fruit):
@@ -79,5 +79,6 @@ if streamlit.button('Add a Fruit to the List'):
   back_from_function = insert_row_snowflake(add_my_fruit)
   streamlit.text(back_from_function)
 
+streamlit.stop()
 
 
